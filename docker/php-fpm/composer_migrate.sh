adcde264a3ca22b813e9a138c315724fdba72f76
Huby Franck <franck.huby@loamok.org>
2021-04-25 15:25:49 +0200

    Début de projet stack docker

--- docker/php-fpm/composer_migrate.sh
+++ docker/php-fpm/composer_migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/sfuxturbo
+cd /var/www/l_wtt
 
 /usr/local/bin/symfony composer install -n
 /usr/local/bin/symfony console doctrine:migrations:migrate --allow-no-migration --no-interaction
